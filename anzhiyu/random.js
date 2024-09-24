aef001579bbd4e9cd4fee723f9e0f65c1311eca4
chwzx <1284905993@qq.com>
2024-09-24 23:02:42 +0800

    Site updated: 2024-09-24 23:02:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2022/03/06/ACM_数论/","2024/06/02/HTTP请求内容/","2022/01/11/Hyperldeger-Fabric环境搭建/","2024/09/24/Hot100-Day1/","2024/06/02/Rust-Warp处理HTTP请求/","2024/05/11/Rust单链表/","2022/07/10/以太坊测试网络Rinkeby使用教程/","2022/03/06/区块链溯源系统环境部署/","2022/07/08/如何使用goblintownNFT智能合约发布属于自己的NFT/","2022/07/08/如何在Linux上部署IPFS/"];function toRandomPost(){
+var posts=["2022/03/06/ACM_数论/","2024/06/02/HTTP请求内容/","2024/09/24/Hot100-Day1/","2022/01/11/Hyperldeger-Fabric环境搭建/","2024/06/02/Rust-Warp处理HTTP请求/","2024/05/11/Rust单链表/","2022/07/10/以太坊测试网络Rinkeby使用教程/","2022/03/06/区块链溯源系统环境部署/","2022/07/08/如何使用goblintownNFT智能合约发布属于自己的NFT/","2022/07/08/如何在Linux上部署IPFS/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
